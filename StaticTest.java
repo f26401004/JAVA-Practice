0bc15aa8a85da77e0ba42c092519f4ff96e4e5c6
f26401004 <f26401004@gmail.com>
2018-03-30 11:13:50 +0800

    Upload the StaticTest practice of java.

---
+++ StaticTest.java
@@ -0,0 +1,8 @@
+
+public class StaticTest {
+    public static void main(String[] args) {
+        int sum = Tool.add(1, 1);
+        System.out.println(sum);
+    }
+}
+
---
+++ Tool.java
@@ -0,0 +1,5 @@
+public class Tool {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
