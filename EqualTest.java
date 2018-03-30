197f5e5ccead322bb22b586851027a8d334c7b09
f26401004 <f26401004@gmail.com>
2018-03-30 11:07:33 +0800

    Upload the equaltest practice of java.

---
+++ EqualTest.java
@@ -0,0 +1,13 @@
+public class EqualTest {
+    public static void main(String[] args) {
+        String a = new String("Java");
+        String b = new String("Java");
+        System.out.println(a);
+        System.out.println(b);
+
+        if (a == b)
+            System.out.println("a == b");
+        if (a.equals(b))
+            System.out.println("a equals to b");
+    }
+}
